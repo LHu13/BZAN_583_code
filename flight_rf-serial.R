cf2a51dcc799e7ad9d3e4d731e6315dd8491f548
Liana <lianali1202@hotmail.com>
2024-04-21 11:49:46 -0400

    Cleaned up code.

--- flight_rf-serial.R
+++ flight_rf-serial.R
@@ -4,59 +4,66 @@ library(dplyr)
 library(tidyr)
 library(MERO)
 library(randomForest)
-library(parsnip)
-library(ranger)
 library(parallel)
 
 ### DATA LOADING 
 
-#load in the dataset
-ds <- open_dataset("C:\\Users\\liana\\OneDrive\\Desktop\\BZAN_583_data\\TEST_itineraries", partitioning = c("flightDate"), unify_schemas = TRUE) 
+#Load in the dataset
+ds <- open_dataset("C:\\Users\\liana\\OneDrive\\Desktop\\BZAN_583_data\\TEST_itineraries", 
+                   partitioning = c("flightDate"), 
+                   unify_schemas = TRUE) 
 
-#clean, format, and prepare data
+#Clean, format, and prepare data
 data <- ds %>%
   collect() %>%
-  filter(isNonStop == "True") %>%
+  filter(isNonStop == "True") %>% #remove all the not nonstop flights
+  #convert time columns into datetime format
   mutate(segmentsArrivalTimeRaw =as.POSIXct(segmentsArrivalTimeRaw, format = "%Y-%m-%dT%H:%M:%OS", tz = "GMT")) %>%
   mutate(segmentsDepartureTimeRaw=as.POSIXct(segmentsDepartureTimeRaw, format = "%Y-%m-%dT%H:%M:%OS", tz = "GMT")) %>%
+  #transforms number columns from character to numeric
   transform(segmentsDurationInSeconds=as.numeric(segmentsDurationInSeconds),
             segmentsDistance=as.numeric(segmentsDistance)) %>%
-  select(-c("segmentsDepartureTimeEpochSeconds",
-            "segmentsArrivalTimeEpochSeconds",
-            "segmentsAirlineCode",
-            "legId",
-            "travelDuration",
-            "fareBasisCode")) %>%
-  drop_na() %>%
+  #drop unnecessary columns
+  select(-c("segmentsDepartureTimeEpochSeconds", #repeat info
+            "segmentsArrivalTimeEpochSeconds", #repeat info
+            "segmentsAirlineCode", #repeat info
+            "legId", #unnecessary
+            "travelDuration", #repeat info
+            "fareBasisCode")) %>% #unnecessary
+  drop_na() %>% #drops the nas
   collect() 
 
 
 ## PREPARE DATA FOR TRAINING AND TESTING
-n <- nrow(data) #counts how many rows there are in the dataset
-
-n_test <- floor(0.2 * n) #gets the number of 20% of the dataset's rows rounded down
-
-i_test <- sample.int(n, n_test) #picks random integers that will be the row indices; chooses n_test rows out of n total rows
-
-train <- data[-i_test, ] #creates the training dataset by excluding the randomly chosen test indices from i_test
-
-test <- data[i_test, ] #creates the training dataset by including the randomly chosen test indices from i_test
+#Counts how many rows there are in the dataset
+n <- nrow(data) 
+#Gets the number of 20% of the dataset's rows rounded down
+n_test <- floor(0.2 * n) 
+#Picks random integers that will be the row indices; chooses n_test rows out of n total rows
+i_test <- sample.int(n, n_test) 
+#Creates the training dataset by excluding the randomly chosen test indices from i_test
+train <- data[-i_test, ] 
+#Creates the training dataset by including the randomly chosen test indices from i_test
+test <- data[i_test, ]
 
 
 
 ## TRAIN THE RANDOM FOREST MODEL
-rf.all = randomForest(totalFare ~ ., #chooses the column being predicted
+#Train random forest model and call it rf.all
+rf.all <- randomForest(totalFare ~ ., #chooses the column being predicted
                       train, #selects the training set to train on
                       ntree = 500, #builds 500 trees
                       norm.votes = FALSE) #disables normalizing of votes among trees
 
 ## PREDICT FOR TEST SET WITH TRAINED RANDOM FOREST MODEL
-pred = predict(rf.all, test) #uses the trained random forest model to predict for the test set
+#Uses the trained random forest model to predict for the test set
+pred <- predict(rf.all, test) 
 
 
 ## CALCULATE THE ACCURACY
-correct = sum(pred == test$totalFare) #counts how many predictions match what it actually is in the dataset
-
-cat("Proportion Correct:", correct/(n_test), "\n") #prints the amount of data correct
-
-cat("RMSE:",RMSE(test$totalFare,pred), "\n") #prints the RMSE
+#Counts how many predictions match what it actually is in the dataset
+correct <- sum(pred == test$totalFare) 
+#Prints the amount of data correct
+cat("Proportion Correct:", correct/(n_test), "\n") 
+#Prints the RMSE
+cat("RMSE:",RMSE(test$totalFare,pred), "\n") 
