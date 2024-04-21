a5c6612c4700db6935009a4322f814012bf635fc
Liana <lianali1202@hotmail.com>
2024-04-21 11:48:59 -0400

    Add flight_rf-parallel code

---
+++ flight_rf-parallel.R
@@ -0,0 +1,84 @@
+#load all the libraries
+library(arrow)
+library(dplyr)
+library(tidyr)
+library(MERO)
+library(randomForest)
+library(parallel)
+
+#Load in the dataset
+ds <- open_dataset("C:\\Users\\liana\\OneDrive\\Desktop\\BZAN_583_data\\TEST_itineraries", 
+                   partitioning = c("flightDate"), 
+                   unify_schemas = TRUE) 
+
+#Clean, format, and prepare data
+data <- ds %>%
+  collect() %>%
+  filter(isNonStop == "True") %>% #remove all the not nonstop flights
+  #convert time columns into datetime format
+  mutate(segmentsArrivalTimeRaw =as.POSIXct(segmentsArrivalTimeRaw, format = "%Y-%m-%dT%H:%M:%OS", tz = "GMT")) %>%
+  mutate(segmentsDepartureTimeRaw=as.POSIXct(segmentsDepartureTimeRaw, format = "%Y-%m-%dT%H:%M:%OS", tz = "GMT")) %>%
+  #transforms number columns from character to numeric
+  transform(segmentsDurationInSeconds=as.numeric(segmentsDurationInSeconds),
+            segmentsDistance=as.numeric(segmentsDistance)) %>%
+  #drop unnecessary columns
+  select(-c("segmentsDepartureTimeEpochSeconds", #repeat info
+            "segmentsArrivalTimeEpochSeconds", #repeat info
+            "segmentsAirlineCode", #repeat info
+            "legId", #unnecessary
+            "travelDuration", #repeat info
+            "fareBasisCode")) %>% #unnecessary
+  drop_na() %>% #drops the nas
+  collect() 
+
+
+## PREPARE DATA FOR TRAINING AND TESTING
+#Counts how many rows there are in the dataset
+n <- nrow(data) 
+#Gets the number of 20% of the dataset's rows rounded down
+n_test <- floor(0.2 * n) 
+#Picks random integers that will be the row indices; chooses n_test rows out of n total rows
+i_test <- sample.int(n, n_test) 
+#Creates the training dataset by excluding the randomly chosen test indices from i_test
+train <- data[-i_test, ] 
+#Creates the training dataset by including the randomly chosen test indices from i_test
+test <- data[i_test, ]
+
+## 
+#commandArgs(TRUE)[2] - gets the second command-line argument as a numeric value
+#Determines the number of CPU cores for parallel processing
+nc <- as.numeric(commandArgs(TRUE)[2])
+#Determines number of trees, splits 500 trees on available CPU cores 
+ntree <- lapply(splitIndices(500, nc), length) 
+
+
+#Define function to train random forest model
+rf <- function(x, train) randomForest(totalFare ~ ., train, ntree=x, norm.votes = FALSE)
+#Applies the random forest training function in parallel on the number of trees
+rf.parts <- mclapply(ntree, #number of trees to be built on a CPU core
+                     rf, #uses the random forest training function rf
+                     train = train, #chooses the training data
+                     mc.cores = nc) #chooses number of CPU cores to be used
+#Combines the results of the parallel trained random forest models into rf.all
+rf.all <- do.call(combine, rf.parts) 
+
+
+#Splits the test data into chunks based on number of CPU cores
+crows <- splitIndices(nrow(test), nc) 
+#Define function to predict on test data
+rfp <- function(x) as.vector(predict(rf.all, test[x, ]))
+#Applies the prediction function in parallel to the chunks of test data
+cpred <- mclapply(crows, #data selection
+                  rfp, #prediction function selection
+                  mc.cores = nc)  #chooses number of CPU cores
+#Combines the predictions from the chunks of data into one vector
+pred <- do.call(c, cpred)                            
+
+
+## CALCULATE THE ACCURACY
+#Counts how many predictions match what it actually is in the dataset
+correct <- sum(pred == test$totalFare) 
+#Prints the amount of data correct
+cat("Proportion Correct:", correct/(n_test), "\n") 
+#Prints the RMSE
+cat("RMSE:",RMSE(test$totalFare,pred), "\n") 
