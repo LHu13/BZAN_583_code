f43d710827b2292b14e1fe4acaaa5f7467795625
Liana <lianali1202@hotmail.com>
2024-04-21 13:56:48 -0400

    Updated code to partition data

--- csv_to_parquet.R
+++ csv_to_parquet.R
@@ -1,5 +1,11 @@
 arrow::install_arrow(verbose = TRUE)
 library(arrow)
 library(dplyr)
-tlc = open_dataset("/projects/bckj/Team3/flight_data_csv/itineraries.csv")
-tlc %>% write_parquet(sink = "/projects/bckj/Team3/flight_data_parquet/itineraries")
+
+flight_data = open_csv_dataset("/projects/bckj/Team3/flight_data_csv/itineraries.csv")
+
+flight_data %>%
+  write_dataset(
+    path = "/projects/bckj/Team3/flight_data_parquet/itineraries",
+    format = c("parquet"),
+    partitioning = c("flightDate"))
