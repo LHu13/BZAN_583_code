201f6746c4ed41a7ff1606c8c93dbcf800c8f9ca
Liana <lianali1202@hotmail.com>
2024-04-21 22:04:30 -0400

    Added more capacity

--- flight_rf_runner.sh
+++ flight_rf_runner.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name utk
 #SBATCH --account=bckj-delta-cpu
 #SBATCH --partition=cpu
-#SBATCH --mem=20g
+#SBATCH --mem=64g
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time 00:10:00
+#SBATCH --time 02:00:00
 #SBATCH -e ./utk.e
 #SBATCH -o ./utk.o
 
