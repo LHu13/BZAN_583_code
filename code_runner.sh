6ea27bcc92cf304087036e6993ad68ad4722a1cd
LHu13 <112121959+LHu13@users.noreply.github.com>
2024-04-24 16:19:40 -0400

    Uploading a running code

---
+++ code_runner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name utk
+#SBATCH --account=bckj-delta-cpu
+#SBATCH --partition=cpu
+#SBATCH --mem=64g
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time 10:00:00
+#SBATCH -e ./utk.e
+#SBATCH -o ./utk.o
+
+pwd
+
+module load r
+module list
+
+#time Rscript flight_rf_serial.R
+#time Rscript flight_rf_parallel.R --args 1
+#time Rscript flight_rf_parallel.R --args 2
+#time Rscript flight_rf_parallel.R --args 4
+time Rscript flight_rf_parallel.R --args 8
+time Rscript flight_rf_parallel.R --args 16
+
