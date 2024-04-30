df63ce422c778d4fa2cb86f7d5519e43308d8e8c
Liana <lianali1202@hotmail.com>
2024-04-30 10:21:35 -0400

    Customized code for flight data.

--- data_rf_mpi_mc.R
+++ data_rf_mpi_mc.R
@@ -2,24 +2,55 @@ suppressMessages(library(pbdMPI))
 
 suppressMessages(library(arrow))
 suppressMessages(library(dplyr))
-suppressMessages(...)
+suppressMessages(library(tidyr))
+suppressMessages(library(MERO))
+suppressMessages(library(randomForest))
+suppressMessages(library(parallel))
 
-ds <- open_dataset("/projects/bckj/<your-parquet-data>")
+
+ds <- open_dataset("/projects/bckj/Team3/flight_data_parquet/itineraries")
+
 get_hive_var = function(ds, var) 
   sub("/.*$", "", sub(paste0("^.*", var, "="), "", ds$files))
-partitions = get_hive_var(ds, "<your-partition-var>")  
 
-my_partitions = partitions[comm.chunk(length(partitions), form = "vector")]
-comm.cat("rank", comm.rank(), "partitions", my_partitions, "\n", all.rank = TRUE)
+partitions = get_hive_var(ds, "flightDate")  
+
+my_partitions = partitions[comm.chunk(length(partitions), 
+                                      form = "vector")]
+
+comm.cat("rank", 
+         comm.rank(), 
+         "partitions", 
+         my_partitions, 
+         "\n", 
+         all.rank = TRUE)
 
 # Read only the data for the partitions in my_partitions
-my_data <- ds %>% filter(flightDate %in% my_partitions) %>% collect()
+my_data <- ds %>% 
+  filter(flightDate %in% my_partitions) %>% 
+  collect()
 
 # Filter, select, mutate, and reduce the data (do separately for debug)
 my_data <- my_data %>%
-  filter(<your-filters>) %>% 
-  select(<your-var-selections>)) %>%
-  drop_na() %>% collect()
+  filter(isNonStop == "True") %>% #remove all the not nonstop flights
+  #drop unnecessary columns
+  select(-c("segmentsDepartureTimeEpochSeconds", #repeat info
+            "segmentsArrivalTimeEpochSeconds", #repeat info
+            "segmentsAirlineCode", #repeat info
+            "legId", #unnecessary
+            "travelDuration", #repeat info
+            "fareBasisCode")) %>% #unnecessary
+  #convert time columns into datetime format
+  mutate(segmentsArrivalTimeRaw =as.POSIXct(segmentsArrivalTimeRaw, format = "%Y-%m-%dT%H:%M:%OS", tz = "GMT")) %>%
+  mutate(segmentsDepartureTimeRaw=as.POSIXct(segmentsDepartureTimeRaw, format = "%Y-%m-%dT%H:%M:%OS", tz = "GMT")) %>%
+  #DROPS ALL OTHER MONTHS BESIDES MAY BC DATA FUNKY
+  filter(as.integer(format(segmentsArrivalTimeRaw, "%m")) %in% c(5)) %>%
+  filter(as.integer(format(segmentsArrivalTimeRaw, "%d")) %in% c(1)) %>%
+  #transforms number columns from character to numeric
+  transform(segmentsDurationInSeconds=as.numeric(segmentsDurationInSeconds),
+            segmentsDistance=as.numeric(segmentsDistance)) %>%
+  drop_na() %>% 
+  collect()
 
 comm.cat(comm.rank(), "dim", dim(my_data), "\n", all.rank = TRUE)
 
@@ -39,8 +70,6 @@ comm.cat(comm.rank(), "dim", dim(data), "\n", all.rank = TRUE)
 comm.print(memuse::Sys.procmem()$size, all.rank = TRUE)
 
 ## Parallel random forest part
-suppressMessages(library(randomForest))
-suppressMessages(library(parallel))
 comm.set.seed(seed = 7654321, diff = FALSE)      #<<
 
 n = nrow(data)
@@ -53,7 +82,11 @@ ntree = 64 # start small for debug
 my_ntree = comm.chunk(ntree, form = "vector", rng = TRUE, seed = 12345)        #<<
 rfc = function(i, mnt, mxn = 6) {
   comm.set.stream(i)                  #<<
-  randomForest(<your-target> ~ ., train, ntree = mnt[i], maxnodes = mxn, norm.votes = FALSE) #<<
+  randomForest(totalFare ~ ., 
+               train, 
+               ntree = mnt[i], 
+               maxnodes = mxn, 
+               norm.votes = FALSE) #<<
 }
 my_rf = mclapply(seq_along(my_ntree), rfc, mnt = my_ntree) #<<
 my_rf = do.call(combine, my_rf)            #<<
@@ -63,7 +96,7 @@ rf_all = do.call(combine, rf_all)          #<<
 my_pred = as.vector(predict(rf_all, my_test))
 
 # correct = allreduce(sum(my_pred == my_test$<your-true-category>))  # classification
-sse = allreduce(sum((my_pred - my_test$<your-target>)^2)) # regression
+sse = allreduce(sum((my_pred - my_test$totalFare)^2)) # regression
 # comm.cat("Proportion Correct:", correct/(n_test), "\n")
 comm.cat("RMSE:", sqrt(rmse/n_test), "\n")
 
