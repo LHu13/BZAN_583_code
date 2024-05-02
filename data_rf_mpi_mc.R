54131d36cec7e865c9dbbc544965d5c0d70d8997
Liana <lianali1202@hotmail.com>
2024-05-02 14:33:59 -0400

    Retracing back to old working code.

--- data_rf_mpi_mc.R
+++ data_rf_mpi_mc.R
@@ -1,4 +1,3 @@
-
 # Load relevant libraries and suppress loading messages.
 suppressMessages(library(pbdMPI))
 suppressMessages(library(arrow))
@@ -6,7 +5,6 @@ suppressMessages(library(dplyr))
 suppressMessages(library(tidyr))
 suppressMessages(library(randomForest))
 suppressMessages(library(parallel))
-
 # Set seed for reproducibility
 comm.set.seed(seed = 7654321, diff = FALSE) 
 
@@ -16,29 +14,22 @@ start_time <- Sys.time()
 ### DATA LOADING ###
 # Load in data from server.
 ds <- open_dataset("/projects/bckj/Team3/flight_data_parquet/itineraries")
-
 # Create function to extract partition information from the file paths in the dataset
 get_hive_var = function(ds, var) # select dataset and partitioning variable
   sub("/.*$", "", sub(paste0("^.*", var, "="), "", ds$files)) # regex to manipulate strings
-
 # Extracts the partition variable values from the dataset
 partitions = get_hive_var(ds, "flightDate")  
-
 # Distributes the partitions evenly across different processors
-# comm.chunk splits the data so each processor works on a different piece of dataset
-my_partitions = partitions[comm.chunk(length(partitions),
+my_partitions = partitions[comm.chunk(length(partitions), #comm.chunk splits the data so each processor works on a different piece of dataset
                                       form = "vector")]
-
 # Print out the MPI rank (i.e. identifier for each processor)
 #and the partitions assigned to each rank
-comm.cat("MPI Rank (ID for each processor)", 
-         comm.rank(), # comm.rank() : gets the MPI rank/identifier for each processor
+comm.cat("rank", 
+         comm.rank(), 
          "partitions", 
          my_partitions, # selects the distributed partitions
          "\n", 
          all.rank = TRUE) # ensures the output is generated from all processors
-
-
 ### DATA CLEANING ###
 # Read only the data for the partitions in my_partitions
 my_data <- ds %>% 
@@ -52,7 +43,6 @@ my_data <- ds %>%
             "travelDuration", #repeat info
             "fareBasisCode")) %>% #unnecessary
   collect()
-
 # Filter, select, mutate, and reduce the data (do separately for debug)
 my_data <- my_data %>%
   #convert time columns into datetime format
@@ -66,98 +56,63 @@ my_data <- my_data %>%
   
   drop_na() %>%#drops the nas
   collect()
-
-# Print dimensions of the dataframe
-comm.cat(comm.rank(), # comm.rank() : gets the MPI rank/identifier for each processor
+comm.cat(comm.rank(), 
          "dim", 
          dim(my_data), "\n", # dim(): outputs dataframe dimensions
          all.rank = TRUE) # ensures the output is generated from all processors
-
-
-
-# Creates a function that gathers all 
-allgather.data.frame = function(df) {
-  
-  cnames = names(df) # gets the column names
-  # gather() : combines the separated data into a single unit; gather columns into key-value pairs
-  # allgather() : same as gather() but for more and mixes it so the results have all the data in multiple portions
-  # do.call : constructs and executes a function call from a name or a function and a list of arguments to be passed to it
-  # Gather all elements of the column across all processes and concatenate them into a single vector
-  df = lapply(df, 
-             function(x) do.call(c, # splits the dataframe into columns that are gathered
-                                 allgather(x))) 
-  df = as.data.frame(df) # turns output back into a dataframe
-  names(df) = cnames # renames the columns to be correct
-  df #returns the gathered dataframes
+## allgather() for data.frames (not in pbdMPI ... yet!)
+allgather.data.frame = function(x) {
+  cnames = names(x) # gets the column names
+  x = lapply(x, 
+             function(x) do.call(c, 
+                                 allgather(x))) # function that 
+  x = as.data.frame(x)
+  names(x) = cnames
+  x
 }
-
-
-# Gathers all the data into one dataset
 data = allgather.data.frame(my_data)
-
-rm(my_data) # removes my_data free up memory
-
+rm(my_data) # free up memory
 ## Check on the result
-comm.cat(comm.rank(), # comm.rank() : gets the MPI rank/identifier for each processor
-         "Data Dimensions (row count, column count) : ", 
-         dim(data), "\n", # dim(): outputs dataframe dimensions
-         all.rank = TRUE) # ensures the output is generated from all processors
-
-
-comm.print("Total Memory Usage: ",
-           memuse::Sys.procmem()$size, #Sys.procmem() :  total memory usage of the current R process
-           all.rank = TRUE)
+comm.cat(comm.rank(), "dim", dim(data), "\n", all.rank = TRUE)
+comm.print(memuse::Sys.procmem()$size, all.rank = TRUE)
 
 
 
 
 end_time <- Sys.time()
 
-cat("\n Data Preparation Time: ", round(end_time-start_time,2), " seconds \n")
+cat("Data Preparation Time: ", round(end_time-start_time,2))
 
 
 
 
 
-### TRAIN PARALLEL RANDOM FOREST ###
-i_samp = sample.int(nrow(data), 1000000)
+## Parallel random forest part
+i_samp = sample.int(nrow(data), 100000)
 data = data[i_samp, ]    # limit to 100k obs for debugging
-
 n = nrow(data)
 n_test = floor(0.2 * n)
 i_test = sample.int(n, n_test)
 train = data[-i_test, ]
 my_test = data[i_test, ][comm.chunk(n_test, form = "vector"), ] 
 rm(data)  # no longer needed, free up memory
-
 ## start with nodesize at 1% of the data and small ntree
-ntree = 500
+ntree = 64
 my_ntree = comm.chunk(ntree, form = "number", rng = TRUE, seed = 12345)
 rF = function(nt, tr) 
-  randomForest(your_target ~ ., data = tr, ntree = nt, nodesize = 10000, norm.votes = FALSE) 
+  randomForest(totalFare ~ ., data = tr, ntree = nt, nodesize = 1000, norm.votes = FALSE) 
 nc = as.numeric(commandArgs(TRUE)[2]) 
 rf = mclapply(seq_len(my_ntree), rF, tr = train, mc.cores = nc)
 rf = do.call(combine, rf)  # reusing rf name to release memory after operation
 rf = allgather(rf) 
 rf = do.call(combine, rf)
-
-
-
-
-
-
-
-
 my_pred = as.vector(predict(rf, my_test))
-
 # correct = allreduce(sum(my_pred == my_test$your_true_category))  # classification
 sse = allreduce(sum((my_pred - my_test$your_target)^2)) # regression
 rmse = sqrt(sse/n_test)
 # comm.cat("Proportion Correct:", correct/(n_test), "\n")
 comm.cat("RMSE:", rmse, "\n")
-
 mean = allreduce(sum(my_test$totalFare)) / n_test
-
 comm.cat("Mean:", mean, "\n")
 comm.cat("Coefficient of Variation:", 100*rmse/mean, "\n")
 
@@ -165,11 +120,7 @@ comm.cat("Coefficient of Variation:", 100*rmse/mean, "\n")
 
 end_time <- Sys.time()
 
-cat("\n Total Time: ", round(end_time-start_time,2), " seconds \n")
-
-results <- data.frame(my_pred,my_test$your_target)
-cat(head(results))
-cat("Code finished running. \n")
-
+cat("Total Time: ", round(end_time-start_time,2))
+cat("Code finished running.")
 
 finalize()
