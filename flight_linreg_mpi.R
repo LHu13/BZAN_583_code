6e940df65d77b4d71e0da83de6c22151616f00a0
Liana <lianali1202@hotmail.com>
2024-05-05 14:37:01 -0400

    Upped sample size for lin reg.

--- flight_linreg_mpi.R
+++ flight_linreg_mpi.R
@@ -8,7 +8,7 @@ suppressMessages(library(lubridate))
 # Set seed for reproducibility
 comm.set.seed(seed = 7654321, diff = FALSE) 
 
-SAMPLE_SIZE <- 5000000
+SAMPLE_SIZE <- 10000000
 
 # TIME IT
 #start_time <- Sys.time()
