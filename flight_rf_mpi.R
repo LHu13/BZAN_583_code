649bd069bab37766f3edb41188597fbe980172e4
Liana <lianali1202@hotmail.com>
2024-05-04 23:11:49 -0400

    Fix

--- flight_rf_mpi.R
+++ flight_rf_mpi.R
@@ -9,7 +9,7 @@ suppressMessages(library(lubridate))
 # Set seed for reproducibility
 comm.set.seed(seed = 7654321, diff = FALSE) 
 
-SAMPLE_SIZE <- 8500000
+SAMPLE_SIZE <- 8000000
 
 # TIME IT
 #start_time <- Sys.time()
